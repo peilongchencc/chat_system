b05322a2cbf9e19d808652f1e0460da97bae82cc
peilongchencc <peilongchencc@163.com>
2024-02-12 09:38:08 +0800

    更新代码

--- langchain_prompt.py
+++
@@ -1,5 +0,0 @@
-from langchain_core.prompts import ChatPromptTemplate
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are world class technical documentation writer."),
-    ("user", "{input}")
-])
--- loguru_test.py
+++
@@ -1,7 +0,0 @@
-from loguru import logger
-
-# 设置日志
-logger.remove()
-logger.add("baidu_llm.log", rotation="1 GB", backtrace=True, diagnose=True, format="{time} {level} {message}")
-
-logger.warning("用户输入为空")
